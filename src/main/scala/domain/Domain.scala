7df2e879e1bc4781fbe13c6215aec317f650e623
spo3920 <silvia.pina@springer.com>
2018-04-13 12:00:41 +0100

    Added mappings.

--- src/main/scala/domain/Domain.scala
+++ src/main/scala/domain/Domain.scala
@@ -1,9 +1,21 @@
 package domain
 
 object Domain {
+  case class XmlEntry(subjects: List[Subject], details: List[RepoDetails])
+
   case class Subject(name: String) extends AnyVal
 
   case class RepoDetails(label: String, webpage: String)
 
-  val subjectToRepoMapping : Map[String, RepoDetails] = Map("test" -> RepoDetails("label", "webpage"))
+  val subjectToRepoMapping : Map[String, List[RepoDetails]] =
+    Map(
+      "health-sciences" -> List(
+          RepoDetails("National Database for Clinical Trials related to Mental Illness", "http://ndct.nimh.nih.gov/"),
+          RepoDetails("The Cancer Imaging Archive", "http://www.cancerimagingarchive.net/"),
+        ),
+      "genomics" -> List(
+        RepoDetails("The European Genome-phenome Archive", "https://www.ebi.ac.uk/ega/")),
+      "protoemics" -> List(
+        RepoDetails("The European Genome-phenome Archive", "https://www.ebi.ac.uk/ega/"))
+    )
 }
